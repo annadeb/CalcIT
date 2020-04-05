5dc5ff437eea19a94835ddd9b6d7b9d55990040f
Anna Dębowska <anna-debowska@o2.pl>
2020-04-05 23:05:03 +0200

    Widok i backend do BMI

---
+++ CalcIt/CalcIt/Controllers/BodyMassIndexController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CalcIt.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CalcIt.Controllers
+{
+    public class BodyMassIndexController : Controller
+    {
+        BodyMassIndexModel bmi;
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CalculateBMI(string weight, string height)
+        {
+            bmi = new BodyMassIndexModel();
+            weight = weight.Replace(".",",");
+            height = height.Replace(".", ",");
+            bmi.Weight = double.Parse(weight);
+            bmi.Height = double.Parse(height);
+            bmi.BMI = bmi.Weight / (bmi.Height * bmi.Height);
+            ViewData["BMI"] = bmi.BMI; 
+            return View("Index");
+        }
+    }
+}
---
+++ CalcIt/CalcIt/Models/BodyMassIndexModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalcIt.Models
+{
+    public class BodyMassIndexModel
+    {
+
+        //[Required]
+        [Range(1, 1000, ErrorMessage = "Waga poza zakresem (1-1000).")]
+        public double Weight { get; set; }
+
+        //[Required]
+        [Range(1, 1000, ErrorMessage = "Wzrost poza zakresem (1-4).")]
+        public double Height { get; set; }
+
+        public double BMI { get; set; }
+    }
+}
