7ffc6432d5f25768b6494268669dcde5a58cdcd6
Łukasz <lukasz.grabny@gmail.com>
2020-04-07 21:55:26 +0200

    Łukasz/CIT-9
    part 1

---
+++ CalcIt/CalcIt/Models/Department.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalcIt.Models
+{
+    public class Department
+    {
+        public int DepartmentID;
+        public string Name {get;set;}
+    }
+}
