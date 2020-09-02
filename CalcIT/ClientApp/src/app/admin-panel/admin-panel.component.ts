b3d392ed7254650e51887d513aa07bbaff53b29c
Anna Dębowska <anna-debowska@o2.pl>
2020-09-02 18:22:24 +0200

    Pobieranie użytkowników w panelu administratora

--- CalcIT/ClientApp/src/app/admin-panel/admin-panel.component.ts
+++ CalcIT/ClientApp/src/app/admin-panel/admin-panel.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-admin-panel',
@@ -6,19 +8,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-panel.component.css']
 })
 export class AdminPanelComponent{
-  Users: User[] = [
-    {    user_id: 1, Name: 'Adam', Surname:'Kowalski', Status:'Nieaktywny'},
-    {user_id: 2, Name: 'Anna', Surname:'Radecka', Status:'Lekarz'},
-    {user_id: 3, Name: 'Karol', Surname:'Woźniak', Status:'Nieaktywny'},
-    {user_id: 4, Name: 'Katarzyna', Surname:'Nowak', Status:'Administrator'}];
+  token:string='';
+  Users: User[] = [];
+   // {    id: '1', userName: 'Adam', Status:'Nieaktywny'},
+   // {id: '2', userName: 'Anna', Status:'Lekarz'},
+   // {id: '3', userName: 'Karol', Status:'Nieaktywny'},
+   // {id: '4', userName: 'Katarzyna', Status:'Administrator'}];
 
-  constructor() { }
+  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string, private activatedRoute: ActivatedRoute ) {
 
+    http.get<User[]>('api/admin/GetUsers').subscribe(result => {
+      this.Users = result;
+      console.log(result)
+    }, error => console.error(error));
 
+    for (let index = 0; index < this.Users.length; index++) {
+      http.get<UserRole>('api/admin/GetUserRoles/'+this.Users[index].id).subscribe(role => {
+        this.Users[index].Status = role.roles;
+        console.log('ROLA!')
+        console.log(role.roles)
+      }, error => console.error(error));
+    }
+  /*  this.Users.forEach(user => {
+      
+      http.get<UserRole>('api/admin/GetUserRoles/'+user.id.toString()).subscribe(role => {
+        user.Status = role.roles;
+        console.log('ROLA!'+role)
+      }, error => console.error(error));
+    });*/
+    
+  }
+  ngOnInit() {
+    this.activatedRoute.queryParams.subscribe((params) => {
+        console.log(params['token']); this.token=params['token'];
+    });
+  }
 }
 interface User {
-  user_id: number;
-   Name: string;
-   Surname: string;
+  id: string;
+  userName: string;
    Status: string;
  }
+ interface UserRole
+ {
+    user: string;
+    roles: string;
+ };
