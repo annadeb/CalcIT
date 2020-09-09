3db40dab397c11153a71b17319f13f4f9189f85a
Anna Dębowska <anna-debowska@o2.pl>
2020-09-10 00:43:41 +0200

    Zapis ról, odświeżanie strony po zapisie

--- CalcIT/ClientApp/src/app/admin-panel/admin-panel.component.ts
+++ CalcIT/ClientApp/src/app/admin-panel/admin-panel.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, Inject } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, Inject, ViewChild } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -10,50 +10,41 @@ import { ActivatedRoute } from '@angular/router';
 export class AdminPanelComponent{
   token:string='';
   Users: User[] = [];
-   // {    id: '1', userName: 'Adam', Status:'Nieaktywny'},
-   // {id: '2', userName: 'Anna', Status:'Lekarz'},
-   // {id: '3', userName: 'Karol', Status:'Nieaktywny'},
-   // {id: '4', userName: 'Katarzyna', Status:'Administrator'}];
+  http: HttpClient;
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string, private activatedRoute: ActivatedRoute ) {
-
+    this.http = http;
     http.get<User[]>('api/admin/GetUsers').subscribe(result => {
       this.Users = result;
       for (let index = 0; index < this.Users.length; index++) {
         http.get<UserRole>('api/admin/GetUserRoles?userId='+ this.Users[index].id ).subscribe(role => {
           this.Users[index].Status = role.roles.result.toString();
-          console.log('ROLA!')
-          console.log(role)
         }, error => console.error(error));
-      console.log(result)
     }}, error => console.error(error));
-
-
-    /*http.get<UserRole>('api/admin/GetUserRoles?userId='+  '8b5a7fa2-7edd-4591-827d-2eafc0a44569').subscribe(role => {
-      console.log('ROLA!')
-      console.log(role.roles.result[0])
-    }, error => console.error(error));
-
-    for (let index = 0; index < this.Users.length; index++) {
-      http.get<UserRole>('api/admin/GetUserRoles?userId='+ this.Users[index].id ).subscribe(role => {
-        this.Users[index].Status = role.roles.result.toString();
-        console.log('ROLA!')
-        console.log(role)
-      }, error => console.error(error)); */
-    }
-  /*  this.Users.forEach(user => {
-      
-      http.get<UserRole>('api/admin/GetUserRoles/'+user.id.toString()).subscribe(role => {
-        user.Status = role.roles;
-        console.log('ROLA!'+role)
-      }, error => console.error(error));
-    });*/
-    
-  
+    }    
+    selectedOption: string;  
+    options = [
+      { name: "NotActive", value: 1 },
+      { name: "Doctor", value: 2 }
+    ]
   ngOnInit() {
     this.activatedRoute.queryParams.subscribe((params) => {
         console.log(params['token']); this.token=params['token'];
     });
+  }
+  public submit(id: number) {
+    console.log(this.selectedOption);
+    console.log(id);
+    const httpOptions = {
+      headers: new HttpHeaders({'Content-Type': 'application/json'})
+    }
+      this.http.post<any>('api/admin/SpecifyUserRole?userId='+id+'&role='+this.selectedOption, JSON, httpOptions).subscribe(
+        (res) => {  if(res.status==200){alert('Przypisanie roli przebiegło pomyślnie');} 
+        else{alert('Coś poszło nie tak. Sprawdź w logach'), console.log(res)}},
+          error => {  if(error.status==200){alert('Przypisanie roli przebiegło pomyślnie');} 
+          else{alert('Coś poszło nie tak. Sprawdź w logach'); console.log(error)}
+      });
+      window.location.reload();
   }
 }
 interface User {
