cf6ec548b04a43f91546137c45d79dc2a3bdc4b1
Anna Dębowska <anna-debowska@o2.pl>
2020-09-12 20:42:12 +0200

    Naprawa parsowania BMI, aktualizacja nugeta

--- CalcIT/ClientApp/src/app/body-mass-index/body-mass-index.component.ts
+++ CalcIT/ClientApp/src/app/body-mass-index/body-mass-index.component.ts
@@ -9,16 +9,24 @@ import { ActivatedRoute } from '@angular/router';
 })
 
 export class BodyMassIndexComponent  {
+  token:string='';
  height: number=180;
   weight: number=70;
   patient_id:number;
+  user_id:string="8d440e1c-afe5-42bd-91f6-ac6a74f90c9e";
   BMIs=[];
   http: HttpClient
-  constructor( http: HttpClient, @Inject('BASE_URL') baseUrl: string, route: ActivatedRoute) { 
+  constructor( http: HttpClient, @Inject('BASE_URL') baseUrl: string, private activatedRoute: ActivatedRoute ) { 
     this.http=http;
-    this.patient_id=route.snapshot.params['patient_id'];
+    this.patient_id=activatedRoute.snapshot.params['patient_id'];
+    //this.user_id=activatedRoute.snapshot.params['user_id'];
     console.log(this.patient_id);
+    //console.log(this.user_id);
   }
+  ngOnInit() {
+    this.activatedRoute.queryParams.subscribe((params) => {
+        console.log(params['token']); this.token=params['token'];
+    });}
   public addWeight(weight:number){
     this.weight=weight;
   }
@@ -32,13 +40,15 @@ export class BodyMassIndexComponent  {
     this.BMIs=[];
     this.BMIs.push(((Math.round((this.weight/(this.height*this.height))*100))/100));
     
-    const formData = new FormData();
+   /* const formData = new FormData();
     formData.append('calculation_data', 'waga: '+ this.weight.toString() + ', wzrost: '+ this.height.toString());
    formData.append('result', ((Math.round((this.weight/(this.height*this.height))*100))/100).toString());
+   const obj = {number: this.patient_id};
+   //const blob = new Blob([JSON.stringify(obj, null, 2)], {type : 'application/json'});
+   formData.append('patient_id', this.patient_id.toString());
    formData.append('calculation_date', new Date().toISOString());
    formData.append('calculation_type', "BMI");
-   formData.append('patient_id', this.patient_id.toString());
-   formData.append('doctor_id', "1");
+   formData.append('user_id', "8d440e1c-afe5-42bd-91f6-ac6a74f90c9e");
    console.log(formData)
    var object = {};
   formData.forEach((value, key) => {
@@ -51,12 +61,13 @@ export class BodyMassIndexComponent  {
       }
       object[key].push(value);
   });
-  var json = JSON.stringify(object);
+  var json = JSON.stringify(object);*/
+    var body = "\{\"calculation_data\": \"waga: "+this.weight+", wzrost: "+this.height+"\", \"result\": \""+((Math.round((this.weight/(this.height*this.height))*100))/100).toString()+"\", \"patient_id\": "+this.patient_id+",\"calculation_date\":\""+new Date().toISOString()+"\",\"calculation_type\":\"BMI\",\"user_id\":\""+this.user_id+"\"\}";
 
    const httpOptions = {
     headers: new HttpHeaders({'Content-Type': 'application/json'})
   }
-    this.http.post<any>('api/Calculation/PostCalculation', json, httpOptions).subscribe(
+    this.http.post<any>('api/Calculation/PostCalculation', body, httpOptions).subscribe(
       (res) => console.log(res),
         (err) => console.log(err)
       );
